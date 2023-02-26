0c51a382d20d6ded1914b9f87351ed48967c8fd7
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-02-26 22:24:23 +0100

    food images showing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FoodService } from '../services/food/food.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  foods:String[] = [];
+  constructor(private foodService:FoodService) { }
 
   ngOnInit(): void {
+    this.foods = this.foodService.getAll();
   }
 
 }
