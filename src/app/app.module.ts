eb7915f42ec9a63da7d248c9a34c628103064517
Raul60rc <34682876+Raul60rc@users.noreply.github.com>
2023-03-12 10:34:31 +0100

    search bar created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 
 
 
@@ -14,6 +15,7 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
+    SearchBarComponent,
     
   ],
   imports: [
